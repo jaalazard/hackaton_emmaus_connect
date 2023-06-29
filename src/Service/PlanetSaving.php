cf08b8a936ca200d572d3608ea6498d3a52343a1
jaalazard <ja.alazard@proton.me>
2023-06-29 18:53:47 +0200

    planet

--- src/Service/PlanetSaving.php
+++ src/Service/PlanetSaving.php
@@ -4,14 +4,14 @@ namespace App\Service;
 
 class PlanetSaving
 {
-    //Production d'un téléphone = 1000L d'eau
+    //Production d'un téléphone = 1000L d'eau.
     public function waterSavings(int $phonesSold): int
     {
         $waterSavings = $phonesSold *1000;
         return $waterSavings;
     }
 
-    //Production d'un téléphone = 44 kilos de roche pour produire 130g de minerais et terres rares
+    //Production d'un téléphone = 44 kilos de roche pour produire 130g de minerais et terres rares.
     public function mineralsSavings(int $phonesSold): int
     {
         $mineralsSavings = $phonesSold * 44;
