5c486178ed8eb9d10f3526b0716f809c6e983240
Noemie.BARRE <noemie.barre24@gmail.com>
2023-06-29 09:45:45 +0200

    phoneFixture

---
+++ src/DataFixtures/PhoneFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Phone;
+use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PhoneFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const PHONECOUNT = 100;
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for($i=0; $i<self::PHONECOUNT; $i++){
+            $phone = new Phone();
+
+            $phone->setIsBlocked($faker->boolean());
+
+            $phone->setModel($this->getReference('model_' . $faker->numberBetween(1, 69)));
+            $phone->setEtat($this->getReference('state_' . $faker->numberBetween(0, 3)));
+            $phone->setUser($this->getReference('user_' . $faker->numberBetween(1, 10)));
+
+            $this->addReference('phone_' . $i, $phone);
+
+            $manager->persist($phone);
+        }
+
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [
+            ModelFixtures::class,
+            StateFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+}
