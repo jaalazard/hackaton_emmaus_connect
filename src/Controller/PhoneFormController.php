ecd739c3953f7db662dad33a271ded8b92267375
jaalazard <ja.alazard@proton.me>
2023-06-29 15:59:12 +0200

    Ajout de price et category

--- src/Controller/PhoneFormController.php
+++ src/Controller/PhoneFormController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Phone;
 use App\Form\PhoneType;
 use App\Repository\PhoneRepository;
+use App\Service\Calculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PhoneFormController extends AbstractController
 {
     #[Route('/ajouterUnTelephone', name: 'app_phone_form')]
-    public function index(Request $request, PhoneRepository $phoneRepository): Response
+    public function index(Request $request, PhoneRepository $phoneRepository, Calculator $calculator): Response
     {
         $phone = new Phone();
         $form = $this->createForm(PhoneType::class, $phone);
@@ -21,6 +22,8 @@ class PhoneFormController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $phone->setUser($this->getUser());
+            $phone->setCategory($calculator->CategoryCalculator($phone->getModel()->getRam(), $phone->getModel()->getMemory(), $phone->isIsBlocked(), $phone->getEtat()));
+            $phone->setPrice($calculator->PriceCalculator($phone->getModel()->getRam(), $phone->getModel()->getMemory(), $phone->isIsBlocked(), $phone->getEtat()));
             $phoneRepository->save($phone, true);
 
             return $this->redirectToRoute('app_phone_form', [], Response::HTTP_SEE_OTHER);
